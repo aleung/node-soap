7a2eb285441d3c48901add075469c259a135b558
Leo Liang <leoliang@gmail.com>
2016-05-05 12:33:00 +0800

    Add test case using ESIM WSDL.

---
+++ test/mytest.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var fs = require('fs'),
+    soap = require('..'),
+    _ = require('lodash'),
+    assert = require('assert');
+
+// soap.createClient(__dirname + '/wsdl/esim/ESIMSubscription.wsdl', function(err, client) {
+//   console.log(JSON.stringify(client.describe()));
+// });
+
